2494b0f8a5dd6e6aad75aaa7ee9fdf628b0ba86e
conner-replogle <95052372+conner-replogle@users.noreply.github.com>
2025-02-24 14:27:01 -0600

    dfdf

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include "Arduino.h"
 #include <RadioLib.h>
 // SX1278 has the following connections:
@@ -7,6 +6,29 @@ SX1278 radio = new Module(8, 6, 7, 3);
 // flag to indicate that a packet was received
 volatile bool rx_flag = false;
 volatile bool txing = false;
+
+// Packet structure (3 bytes overhead):
+// [1 byte sequence number][1 byte total packets][1 byte current packet size][payload...]
+#define MAX_PACKET_SIZE 63
+#define PACKET_OVERHEAD 3
+#define MAX_PAYLOAD_SIZE (MAX_PACKET_SIZE - PACKET_OVERHEAD)
+#define MAX_MESSAGE_SIZE 1024
+
+uint8_t serialInBuf[MAX_MESSAGE_SIZE];
+size_t serialInBufLen = 0;
+uint8_t outBuf[MAX_PACKET_SIZE];
+
+// Receive buffer management
+uint8_t rxMessageBuf[MAX_MESSAGE_SIZE];
+uint8_t rxPacketTracker[16]; // Bitmap to track received packets
+uint8_t currentMessageTotalPackets = 0;
+uint8_t receivedPacketsCount = 0;
+
+void resetRxBuffers() {
+    memset(rxPacketTracker, 0, sizeof(rxPacketTracker));
+    currentMessageTotalPackets = 0;
+    receivedPacketsCount = 0;
+}
 
 // this function is called when a complete packet
 // is received by the module
@@ -97,47 +119,95 @@ void setup()
     }
 }
 
-uint8_t serialInBuf[64];
-size_t serialInBufLen = 0;
-uint8_t outBuf[64];
+void sendPackets(uint8_t* data, size_t len) {
+    uint8_t totalPackets = (len + MAX_PAYLOAD_SIZE - 1) / MAX_PAYLOAD_SIZE;
+    uint8_t packet[MAX_PACKET_SIZE];
+    
+    for (uint8_t seq = 0; seq < totalPackets; seq++) {
+        size_t payloadSize = min(MAX_PAYLOAD_SIZE, len - (seq * MAX_PAYLOAD_SIZE));
+        
+        packet[0] = seq;  // Sequence number
+        packet[1] = totalPackets;  // Total packets
+        packet[2] = payloadSize;  // Current packet size
+        
+        memcpy(packet + PACKET_OVERHEAD, 
+               data + (seq * MAX_PAYLOAD_SIZE), 
+               payloadSize);
+        
+        txing = true;
+        int state = radio.transmit(packet, payloadSize + PACKET_OVERHEAD);
+        Serial.print(F("[DEBUG] Sent Packet "));
+        Serial.print(seq + 1);
+        Serial.print(F("/"));
+        Serial.println(totalPackets);
+        
+        delay(10); 
+    }
+    txing = false;
+    radio.startReceive();
+}
+
+void handleReceivedPacket(uint8_t* data, size_t len) {
+    if (len < PACKET_OVERHEAD) return;
+    
+    uint8_t seq = data[0];
+    uint8_t totalPackets = data[1];
+    uint8_t packetSize = data[2];
+    
+
+
+    // If this is the first packet of a new message or we need to restart
+    if (currentMessageTotalPackets == 0 || totalPackets != currentMessageTotalPackets) {
+        Serial.println(F("[DEBUG] Starting new message"));
+        resetRxBuffers();
+        currentMessageTotalPackets = totalPackets;
+    }
+    Serial.print(F("[DEBUG] Received Packet "));
+    Serial.print(seq + 1);
+    Serial.print(F("/"));
+    Serial.println(totalPackets);
+    if (!(rxPacketTracker[seq/8] & (1 << (seq%8)))) {
+        // New packet
+        memcpy(rxMessageBuf + (seq * MAX_PAYLOAD_SIZE), 
+               data + PACKET_OVERHEAD, 
+               packetSize);
+        rxPacketTracker[seq/8] |= (1 << (seq%8));
+        receivedPacketsCount++;
+        
+      
+        
+        if (receivedPacketsCount == totalPackets) {
+            // Message complete
+            size_t totalLen = (totalPackets - 1) * MAX_PAYLOAD_SIZE + packetSize;
+            Serial.print(F("[PACKET RX]"));
+            Serial.write(rxMessageBuf, totalLen);
+            Serial.print('\n');
+            resetRxBuffers();
+        }
+    }
+}
 
 void loop()
 {
     if (rx_flag)
     {
-        Serial.println("[DEBUG] Handler called");
-        int state = radio.readData(outBuf, 63);
-        if (state != RADIOLIB_ERR_NONE)
+        int state = radio.readData(outBuf, MAX_PACKET_SIZE);
+        if (state == RADIOLIB_ERR_NONE)
         {
-            Serial.print("[DEBUG] RX ERROR, state: ");
-            Serial.print(state);
-            Serial.print('\n');
-        }
-        else
-        {
-            Serial.print("[PACKET RX]");
-            Serial.write(outBuf, 63);
-            Serial.print('\n');
+            handleReceivedPacket(outBuf, MAX_PACKET_SIZE);
         }
         rx_flag = false;
     }
 
-    // Transmit every 64 bytes, or on a newline char
     int sIn = Serial.read();
     if (sIn != -1)
     {
         serialInBuf[serialInBufLen++] = sIn;
-        // /Serial.print((char)sIn);
-        if (serialInBufLen == 63 || (char)sIn == '\n')
+        
+        if (serialInBufLen >= MAX_MESSAGE_SIZE || (char)sIn == '\n')
         {
-            txing = true;
-            int state = radio.transmit(serialInBuf, serialInBufLen);
-            Serial.print("[DEBUG] Sent Packet, state ");
-            Serial.print(state);
-            Serial.print('\n');
+            sendPackets(serialInBuf, serialInBufLen);
             serialInBufLen = 0;
-            txing = false;
-            radio.startReceive();
         }
     }
 }
